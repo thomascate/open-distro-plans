59d35c7e8d84a5583a6889ff3069efbe2c92c5fe
echohack <echohack@users.noreply.github.com>
2019-04-02 18:15:07 -0700

    init commit
    Signed-off-by: echohack <echohack@users.noreply.github.com>

---
+++ filebeat/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=filebeat
+pkg_origin=open-distro
+pkg_version=6.5.4
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_deps=(core/glibc)
+pkg_build_deps=(
+  core/go
+  core/git
+  core/make
+  core/gcc
+)
+pkg_bin_dirs=(bin)
+pkg_binds_optional=(
+  [kibana]="port"
+  [elasticsearch]="http-port"
+  [logstash]="port"
+)
+pkg_description="Lightweight shipper for logfiles"
+pkg_upstream_url="https://www.elastic.co/products/beats/filebeat"
+
+do_download() {
+  GOPATH="$(dirname "${HAB_CACHE_SRC_PATH}")"
+  export GOPATH
+  go get github.com/elastic/beats/filebeat
+  pushd "${HAB_CACHE_SRC_PATH}/github.com/elastic/beats/filebeat" > /dev/null
+  git checkout "v${pkg_version}"
+  popd > /dev/null
+}
+
+do_unpack() {
+  return 0
+}
+
+do_build() {
+  pushd "${HAB_CACHE_SRC_PATH}/github.com/elastic/beats/filebeat" > /dev/null
+  make
+  popd > /dev/null
+}
+
+do_install() {
+  install -D "${HAB_CACHE_SRC_PATH}/github.com/elastic/beats/filebeat/${pkg_name}" "${pkg_prefix}/bin/${pkg_name}"
+}
